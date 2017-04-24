d7a2edbc8cef434b5e16fd2e28997b79d2c5b1a8
Vojta Oupicky <voj.oup@gmail.com>
2017-04-24 18:03:45 +0200

    Uprava funkcnosti
    Pridana kontrola zadani prave jednoho argumentu.
    Pokud neni zadan zadny argument, nebo jich je prilis mnoho program
    skonci s chybou.
    
    TODO:
        Defaultni dobu odpoctu
        Prepinace pro vlastni text upozorneni
        Prepinac pro valstni dobu odpoctu

--- odpocet.sh
+++ odpocet.sh
@@ -2,4 +2,11 @@
  
 # v argumentu dostane pocet vterin kolik ma pockat
 # po skonceni odpoctu posle systemovou notifikaci s textem "Odpocet skoncil"
-sleep "$1" && notify-send "Odpocet skoncil"
+if [ $# -eq 1 ]
+then 
+    sleep "$1" && notify-send "Odpocet skoncil"
+else
+   echo "Nezadan zadny argument!"
+   echo "Zadej prave jedno cislo"
+   exit 1;
+fi
